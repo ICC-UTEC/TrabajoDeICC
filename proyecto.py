bb577ad0f0d6e3c00848da3acbeefa8ff5182604
alberto.julian@utec.edu.pe <alberto.julian@utec.edu.pe>
2018-06-03 17:39:58 -0500

    Con fe v;

--- proyecto.py
+++ proyecto.py
@@ -1,7 +1,11 @@
-n=int(input('Ingrese su rango de suma:'))
-suma=0
-for i in range(0,n):
-    i=i+1
-    suma=suma+i
-print(suma)
-
+n=int(input('Ingrese el rango de valores que desee:'))
+sumapar=0
+sumaimpar=0
+for i in range(n):
+    k=int(input("Ingrese el número: "))
+    if k//2>=0 and k%2==0:
+        sumapar=sumapar+(k**2)
+    else:
+        sumaimpar=sumaimpar+(k**2)
+print(sumapar)
+print(sumaimpar)
