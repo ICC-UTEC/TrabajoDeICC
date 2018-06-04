0d1db897184766c02675a755cf91c18f6b48159d
renzo2108 <39784490+renzo2108@users.noreply.github.com>
2018-06-03 22:50:05 -0500

    create new listas in program

--- proyecto.py
+++ proyecto.py
@@ -1,11 +1,27 @@
-n=int(input('Ingrese el rango de valores que desee:'))
+n=int(input('Ingrese el rango de valores :'))
 sumapar=0
 sumaimpar=0
 for i in range(n):
-    k=int(input("Ingrese el número: "))
+    k=int(input("Ingrese los números: "))
     if k//2>=0 and k%2==0:
         sumapar=sumapar+(k**2)
     else:
         sumaimpar=sumaimpar+(k**2)
 print(sumapar)
 print(sumaimpar)
+
+#listas alternas
+n = int(input("ingres el numero de numeros:"))
+arreglo1 = []
+arreglo2 = []
+
+for i in range(n * 2):
+    valor = int(input("Ingrese el numero de valores:"))
+
+    if i % 2 == 0:
+        arreglo1.append(valor)
+    else:
+        arreglo2.append(valor)
+
+suma_minimos = min(arreglo1) + min(arreglo2)
+print(suma_minimos)
